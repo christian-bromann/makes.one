55abe402a36c58965770a917506716a01e165bcf
christian-bromann <contact@christian-bromann.com>
2016-09-25 21:44:04 +0200

    finish slideshow animation

--- js/elements/slideshow.jsx
+++ js/elements/slideshow.jsx
@@ -6,6 +6,8 @@ import SVG from './svg.jsx'
 import Zoom from '../lib/zoom'
 import { checkStatus, parseJSON } from '../utils/request'
 
+const SLIDE_ANIM_TIMEOUT = 500
+
 export default class Slideshow extends React.Component {
     get displayName () {
         return 'Slideshow'
@@ -24,10 +26,15 @@ export default class Slideshow extends React.Component {
         super(props)
         this.zoom = new Zoom()
         this.url = `/wp-json/makes/v1/fields/${this.props.post.id}`
-        this.state = { posts: [], displayButtons: false }
+        this.state = { posts: [], displayButtons: false, next: null, in: null, current: 0 }
+        this.childElements = []
     }
 
     toggleZoom (e) {
+        if (['svg', 'path'].indexOf(e.target.tagName) > -1) {
+            return
+        }
+
         this.props.setActivePost(!this.props.isActive ? this.props.post.id : null)
         this.zoom.to({ element: e.target }).then((hasZoomedOut) => {
             if (!hasZoomedOut) {
@@ -53,13 +60,31 @@ export default class Slideshow extends React.Component {
     }
 
     prev (e) {
-        e.preventDefault()
-        console.log('prev', this.props.post.id)
+        this.setState({
+            out: this.state.current,
+            in: (this.childElements.length + ((this.state.current - 1) % this.childElements.length)) % this.childElements.length
+        })
+        setTimeout(() => {
+            this.setState({
+                out: null,
+                in: null,
+                current: this.state.in
+            })
+        }, SLIDE_ANIM_TIMEOUT)
     }
 
     next (e) {
-        e.preventDefault()
-        console.log('next', this.props.post.id)
+        this.setState({
+            out: this.state.current,
+            in: (this.childElements.length + ((this.state.current + 1) % this.childElements.length)) % this.childElements.length
+        })
+        setTimeout(() => {
+            this.setState({
+                out: null,
+                in: null,
+                current: this.state.in
+            })
+        }, SLIDE_ANIM_TIMEOUT)
     }
 
     render () {
@@ -69,8 +94,29 @@ export default class Slideshow extends React.Component {
             'item'
         )
 
-        const childElements = this.state.posts.map((post, i) => {
+        const posts = this.state.posts.slice()
+        posts.unshift({
+            file: this.props.post._embedded['wp:featuredmedia'][0].source_url.slice(8),
+            image_meta: { title: this.props.post.title.rendered },
+            type: 'image'
+        })
+
+        this.childElements = posts.map((post, i) => {
             let postContent
+            const classNames = ['slide', post.type]
+            let direction = this.state.in > this.state.out
+
+            if (Math.abs(this.state.in - this.state.out) !== 1) {
+                direction = !direction
+            }
+
+            if (i === this.state.current) {
+                classNames.push('visible')
+            }
+            if (i === this.state.out || i === this.state.in) {
+                classNames.push('slide' + (direction ? 'Left' : 'Right'))
+            }
+
             switch (post.type) {
             case 'image':
                 postContent = <img src={`/assets/${post.file}`} title={post.image_meta.title} />
@@ -80,7 +126,7 @@ export default class Slideshow extends React.Component {
             }
 
             return (
-                <a className={`slide ${post.type}`} href={`#!/${this.props.post.slug}`} key={i}>
+                <a className={classnames(classNames)} href={`#!/${this.props.post.slug}`} key={i}>
                     {postContent}
                 </a>
             )
@@ -88,20 +134,29 @@ export default class Slideshow extends React.Component {
 
         return (
             <article className={itemClasses} key={this.props.post.id} onClick={this.toggleZoom.bind(this)}>
-                <button className={'left'} onClick={this.prev.bind(this)}>
-                    <SVG name={'arrow_left'} />
-                </button>
+                {(() => {
+                    if (this.childElements.length > 1) {
+                        return (
+                            <button className={'left'} onClick={this.prev.bind(this)}>
+                                <SVG name={'arrow_left'} />
+                            </button>
+                        )
+                    }
+                })()}
 
                 <div className={'content'}>
-                    <a className={'slide image visible'} href={`#!/${this.props.post.slug}`}>
-                        <img alt={this.props.post.title.rendered} src={this.props.post._embedded['wp:featuredmedia'][0].source_url} title={this.props.post.title.rendered}></img>
-                    </a>
-                    {childElements}
+                    {this.childElements}
                 </div>
 
-                <button className={'right'} onClick={this.prev.bind(this)}>
-                    <SVG name={'arrow_right'} />
-                </button>
+                {(() => {
+                    if (this.childElements.length > 1) {
+                        return (
+                            <button className={'right'} onClick={this.next.bind(this)}>
+                                <SVG name={'arrow_right'} />
+                            </button>
+                        )
+                    }
+                })()}
             </article>
         )
     }
